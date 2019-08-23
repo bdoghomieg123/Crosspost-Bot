4d17100f4c8f7c45702e96c0462cf6a0d44173ba
Ben <capnben2009@hotmail.com>
2019-08-23 14:08:59 -0400

    removed redundant/unused modules

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #Sub A IndiaSpeaks  Sub B - IndiaSpeaksTrending
-import praw
-import os
-import time
-from common import clear
+import praw #the Python Reddit API Wrapper
 
 reddit = praw.Reddit('bot1')
 while True:
-    subreddit = reddit.subreddit('IndiaSpeaks')
+    subreddit = reddit.subreddit('IndiaSpeaks') #mofify the text inside the quotes to change where to crosspost FROM
     for submission in subreddit.hot(limit=2):
-        if submission.stickied:
-            submission.crosspost(subreddit="indiaspeakstrending",send_replies=False)
+        if submission.stickied: 
+            crosspost_subreddit = "indiaspeakstrending" #change this to change where to crosspost TO
+            submission.crosspost(subreddit=crosspost_subreddit.lower(),send_replies=False)
             exit()
